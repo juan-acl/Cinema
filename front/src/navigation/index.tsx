8479610017cf12650c64b4652d2f0d6f3714a70c
unknown <dev.juan.eloper@gmail.com>
2024-03-28 21:15:41 -0600

    Options nav

--- front/src/navigation/index.tsx
+++ front/src/navigation/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "@components/home";
 
@@ -6,7 +7,13 @@ const TabNavigation = createBottomTabNavigator();
 const Tab_Navigation = () => {
     return (
         <TabNavigation.Navigator>
-            <TabNavigation.Screen name="Home_Screen" component={Home} />
+            <TabNavigation.Screen
+                name="Home_Screen"
+                component={Home}
+                options={{
+                    title: "Home",
+                    headerShown: false
+                }} />
         </TabNavigation.Navigator>
     )
 }
