0215e16bc8eb9e056537ac5b42cd82a84e15f51c
unknown <dev.juan.eloper@gmail.com>
2024-03-29 22:13:44 -0600

    Fix title

--- front/src/components/home/index.tsx
+++ front/src/components/home/index.tsx
@@ -10,38 +10,37 @@ const Home = () => {
         "movie3": require("../../../assets/movie-3.png"),
     };
     return (
-        <SafeAreaView className="flex-1 bg-customGray items-center justify-center" >
-            <View >
-                <View className="flex-1  " >
-                    <Text className="text-white font-semibold" >Trending movies</Text>
-                </View>
+        <SafeAreaView className="flex-1 bg-customGray items-center justify-center">
+            <View className="absolute top-20 fon w-full flex items-center justify-center z-10">
+                <Text className="text-white text-4xl font-semibold">Trending movies</Text>
+            </View>
+            <View className="flex-1 items-center justify-center">
                 <Corousel
                     data={["movie1", "movie2", "movie3"]}
-                    renderItem={({ item, index }) => {
-                        return (
-                            <View>
-                                <Image
-                                    key={index}
-                                    className="rounded-3xl"
-                                    style={{ width: width * 0.6, height: height * 0.4 }}
-                                    source={images[item]}
-                                />
-                            </View>
-                        )
-                    }}
+                    renderItem={({ item, index }) => (
+                        <View>
+                            <Image
+                                key={index}
+                                className="rounded-3xl"
+                                style={{ width: width * 0.6, height: height * 0.4 }}
+                                source={images[item]}
+                            />
+                        </View>
+                    )}
                     sliderWidth={width * 0.9}
                     itemWidth={width * 0.62}
                     firstItem={1}
                     inactiveSlideOpacity={0.2}
-                    loop={true} // Repetir el Carousel al llegar al final
-                    autoplay={true} // Reproducir automáticamente las imágenes
+                    loop={true}
+                    autoplay={true}
                     autoplayInterval={3000}
                     layout={'default'}
                     slideStyle={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
                 />
             </View>
         </SafeAreaView>
-    )
+    );
+
 }
 
 export default Home;
