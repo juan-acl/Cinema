ca2d8c7f2e837a7b43d170bafce6022d96fa85d0
unknown <dev.juan.eloper@gmail.com>
2024-03-30 18:31:51 -0600

    Fix api

--- back/index.js
+++ back/index.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const app = require("./config/app");
 const mongo_configuration = require("./config/mongoDB");
+const PORT = 3000;
 
 mongo_configuration.initialize_server();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
