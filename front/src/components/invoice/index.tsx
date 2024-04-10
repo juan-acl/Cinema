08c76addd72724fa60f3b7a286e19205fd8e388e
unknown <dev.juan.eloper@gmail.com>
2024-04-09 19:41:01 -0600

    Invoice

--- front/src/components/invoice/index.tsx
+++ front/src/components/invoice/index.tsx
@@ -3,12 +3,11 @@ import { Box, VStack, Heading, Divider, Text, NativeBaseProvider } from 'native-
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useSelector } from 'react-redux';
 import { RootState } from '@redux/configureStore';
+import Logger from '../../utils/logger';
 
 const Invoice = () => {
     const invoice = useSelector((state: RootState) => state.cinema.invoice)
-    const invoiceItems = [
-        { title: 'Factura de asientos', quantity: 2, price: '$20', seats: [1, 2], priceUnique: 75.00 },
-    ];
+    Logger.error({ invoice })
 
     return (
         <SafeAreaView className="flex-1 justify-center bg-customGray">
@@ -16,15 +15,13 @@ const Invoice = () => {
                 <Box safeArea flex={1} p="2" w="90%" mx='auto' className="mt-10 " >
                     <Heading size="lg" color='primary.500'>Factura</Heading>
                     <Divider my="2" />
-                    {invoiceItems.map((item, i) => (
-                        <VStack key={i} space={2} mt="5" className="rounded-2xl border border-white ">
-                            <Text className="text-2xl text-white m-1 p-1">{item.title}</Text>
-                            <Text className="text-2xl text-white m-1 p-1">Precio por unidad: Q.{item.priceUnique}.00</Text>
-                            <Text className="text-2xl text-white m-1 p-1">Asientos totales: {item.quantity}</Text>
-                            <Text className="text-2xl text-white m-1 p-1">Total: {item.price}</Text>
-                            <Text className="text-2xl text-white m-1 p-1">No. de asientos: {item.seats.join(", ")}</Text>
-                        </VStack>
-                    ))}
+                    <VStack key={1} space={2} mt="5" className="rounded-2xl border border-white ">
+                        <Text className="text-2xl text-white m-1 p-1">Pelicula: {invoice.nameMovie}</Text>
+                        <Text className="text-2xl text-white m-1 p-1">Precio por unidad: Q.75.00</Text>
+                        <Text className="text-2xl text-white m-1 p-1">Asientos totales: {invoice.no_seats.length}</Text>
+                        <Text className="text-2xl text-white m-1 p-1">Total: {invoice.no_seats.length * 75}</Text>
+                        <Text className="text-2xl text-white m-1 p-1">No. de los asientos: {invoice.no_seats.join(", ")}</Text>
+                    </VStack>
                 </Box>
             </NativeBaseProvider>
         </SafeAreaView>
