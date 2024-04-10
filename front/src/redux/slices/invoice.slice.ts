ba0457b30954038007914830048d893b74e06607
unknown <dev.juan.eloper@gmail.com>
2024-04-10 00:07:23 -0600

    Redux invoices

---
+++ front/src/redux/slices/invoice.slice.ts
@@ -0,0 +1,28 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+import { setShowLoader } from './loader.slice'
+
+
+interface Invoice {
+    _id: String
+    user: String
+    no_seat: number[]
+    total: number
+}
+
+interface Params {
+    idUser: String
+}
+
+export const getInvoicesById = createAsyncThunk('invoice/invoices-by-user', async ({ idUser }: Params, thunkAPI) => {
+    try {
+        thunkAPI.dispatch(setShowLoader(true));
+        const response = await axios.post(process.env.API + 'invoice/invoices-by-user', { idUser })
+        return response.data
+    } catch (error) {
+        console.log('Error getInvoicesById', { error })
+        return []
+    } finally {
+        thunkAPI.dispatch(setShowLoader(false))
+    }
+})
