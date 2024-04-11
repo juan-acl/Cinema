0506579a0ba403751bad2866b334c507f7a7f23f
unknown <dev.juan.eloper@gmail.com>
2024-04-10 21:17:10 -0600

    Service front

--- front/src/components/myReservations/index.tsx
+++ front/src/components/myReservations/index.tsx
@@ -2,6 +2,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Text } from "react-native";
 
 const MyReservations = () => {
+
+    -const geyMyRerservations = async () => {
+        try {
+
+        } catch (error) {
+            console.log('Error getMyReservations', { error })
+            return []
+        }
+    }
+
     return (
         <SafeAreaView className="bg-customGray flex-1" >
             <Text>My Reservations</Text>
