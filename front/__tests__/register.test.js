d82876b2ab2f552161cf4e433304cd260002db61
unknown <dev.juan.eloper@gmail.com>
2024-04-23 14:03:07 -0600

    Test submit

--- front/__tests__/register.test.js
+++ front/__tests__/register.test.js
@@ -1,18 +1,39 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { render } from "@testing-library/react-native";
-import { Provider } from "react-redux"; // Importa Provider desde react-redux
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
+import { Provider } from "react-redux";
 import Login from "@components/login";
-import { store } from "@redux/configureStore"; // Importa tu tienda de Redux
+import { store } from "@redux/configureStore";
+import { act } from "react-test-renderer";
 
 describe("Login component", () => {
-  it("renders correctly", () => {
-    render(
+  let getByPlaceholderText, getByTestId;
+
+  beforeEach(() => {
+    const rendered = render(
       <Provider store={store}>
         <NavigationContainer>
           <Login />
         </NavigationContainer>
       </Provider>,
     );
+    getByPlaceholderText = rendered.getByPlaceholderText;
+    getByTestId = rendered.getByTestId;
+  });
+
+  test("renders correctly", () => {
+    const emailInput = getByPlaceholderText("Correo electrónico");
+    expect(emailInput).toBeTruthy();
+  });
+
+  test("submits login form", async () => {
+    await waitFor(() => {
+      expect(getByTestId("submit-login")).toBeTruthy();
+    });
+
+    const loginButton = getByTestId("submit-login");
+    await act(async () => {
+      fireEvent.press(loginButton);
+    });
   });
 });
--- front/src/components/login/index.tsx
+++ front/src/components/login/index.tsx
@@ -2,9 +2,9 @@ import React, { useCallback } from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Input, Button } from "react-native-elements";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import { useFormik } from "formik";
-import PageLoader from "@components/loader"
+import PageLoader from "@components/loader";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "@redux/configureStore";
 import { log_in } from "@redux/slices/user.slice";
@@ -12,110 +12,114 @@ import { useFocusEffect } from "@react-navigation/native";
 import { useNavigation } from "@react-navigation/native";
 
 const Login: React.FC = () => {
+  const navigation = useNavigation();
+  const dispatch: AppDispatch = useDispatch();
+  const isLoading = useSelector((state: RootState) => state.pageLoader.loading);
 
-    const navigation = useNavigation();
-    const dispatch: AppDispatch = useDispatch();
-    const isLoading = useSelector((state: RootState) => state.pageLoader.loading);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validateOnBlur: true,
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .required("Este campo es requerido")
+        .email("El correo electrónico no es válido"),
+      password: Yup.string().required("Este campo es requerido"),
+    }),
+    onSubmit: async (values) => {
+      const { email, password } = values;
+      let response = await dispatch(log_in({ email, password }));
+      if (response.payload == "success") {
+        setTimeout(() => {
+          navigation.navigate("Cinema_Navigation" as never);
+        }, 1000);
+      }
+    },
+  });
 
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            password: ""
-        },
-        validateOnBlur: true,
-        validationSchema: Yup.object({
-            email: Yup.string().required('Este campo es requerido').email('El correo electrónico no es válido'),
-            password: Yup.string().required('Este campo es requerido')
-        }),
-        onSubmit: async (values) => {
-            const { email, password } = values
-            let response = await dispatch(log_in({ email, password }))
-            if (response.payload == 'success') {
-                setTimeout(() => {
-                    navigation.navigate('Cinema_Navigation' as never)
-                }, 1000)
-            }
-        }
-    })
+  const onChangeEmail = (email: string) => {
+    formik.setFieldValue("email", email);
+  };
 
-    const onChangeEmail = (email: string) => {
-        formik.setFieldValue('email', email)
-    }
+  const onChangePassword = (email: string) => {
+    formik.setFieldValue("password", email);
+  };
 
-    const onChangePassword = (email: string) => {
-        formik.setFieldValue('password', email)
-    }
+  useFocusEffect(
+    useCallback(() => {
+      return () => formik.resetForm();
+    }, []),
+  );
 
-    useFocusEffect(
-        useCallback(() => {
-            return () => formik.resetForm()
-        }, [])
-    )
-
-    return (
-        <SafeAreaView className="flex-1 justify-center bg-customGray" >
-            {isLoading
-                ?
-                <PageLoader />
-                :
-                <>
-                    <View className="items-center" >
-                        <Text className="text-white text-3xl mb-10" >Iniciar sesi&oacute;n</Text>
-                    </View>
-                    <View>
-                        <Input
-                            value={formik.values.email}
-                            placeholder="Correo electrónico"
-                            className="border-white w-full text-white"
-                            onChangeText={(email) => onChangeEmail(email)}
-                        />
-                        <Text className="text-red-500 ml-3" >{formik.errors.email}</Text>
-                        <Input
-                            value={formik.values.password}
-                            placeholder="Contraseña"
-                            secureTextEntry={true}
-                            onChangeText={(password) => onChangePassword(password)}
-                            className="border-white w-full text-white"
-                        />
-                        <Text className="text-red-500 ml-3" >{formik.errors.password}</Text>
-                    </View>
-                    <View className="items-center flex-row justify-center m-5" >
-                        <Button
-                            title="Iniciar sesi&oacute;n"
-                            onPress={() => {
-                                formik.handleSubmit()
-                            }}
-                            buttonStyle={{
-                                backgroundColor: 'black',
-                                borderColor: 'white',
-                                borderRadius: 30,
-                            }}
-                            containerStyle={{
-                                width: 150
-                            }}
-                            titleStyle={{ fontWeight: 'bold' }}
-                        />
-                        <Button
-                            title="Cancelar"
-                            buttonStyle={{
-                                backgroundColor: 'black',
-                                borderColor: 'white',
-                                borderRadius: 30,
-                            }}
-                            containerStyle={{
-                                width: 150,
-                                marginLeft: 11,
-                            }}
-                            titleStyle={{ fontWeight: 'bold' }}
-                            onPress={() => {
-                                formik.resetForm()
-                            }}
-                        />
-                    </View>
-                </>
-            }
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView className="flex-1 justify-center bg-customGray">
+      {isLoading ? (
+        <PageLoader />
+      ) : (
+        <>
+          <View className="items-center">
+            <Text className="text-white text-3xl mb-10">
+              Iniciar sesi&oacute;n
+            </Text>
+          </View>
+          <View>
+            <Input
+              value={formik.values.email}
+              placeholder="Correo electrónico"
+              className="border-white w-full text-white"
+              onChangeText={(email) => onChangeEmail(email)}
+            />
+            <Text className="text-red-500 ml-3">{formik.errors.email}</Text>
+            <Input
+              value={formik.values.password}
+              placeholder="Contraseña"
+              secureTextEntry={true}
+              onChangeText={(password) => onChangePassword(password)}
+              className="border-white w-full text-white"
+            />
+            <Text className="text-red-500 ml-3">{formik.errors.password}</Text>
+          </View>
+          <View className="items-center flex-row justify-center m-5">
+            <Button
+              title="Iniciar sesi&oacute;n"
+              testID="submit-login"
+              onPress={() => {
+                formik.handleSubmit();
+              }}
+              buttonStyle={{
+                backgroundColor: "black",
+                borderColor: "white",
+                borderRadius: 30,
+              }}
+              containerStyle={{
+                width: 150,
+              }}
+              titleStyle={{ fontWeight: "bold" }}
+            />
+            <Button
+              title="Cancelar"
+              buttonStyle={{
+                backgroundColor: "black",
+                borderColor: "white",
+                borderRadius: 30,
+              }}
+              containerStyle={{
+                width: 150,
+                marginLeft: 11,
+              }}
+              titleStyle={{ fontWeight: "bold" }}
+              onPress={() => {
+                formik.resetForm();
+              }}
+            />
+          </View>
+        </>
+      )}
+    </SafeAreaView>
+  );
+};
 
 export default Login;
+
